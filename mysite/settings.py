13bf3f23920a758762962614922d37e006649a90
Ordo <jaordonezdeleon2007@elvallecolegio.edu.gt>
2024-07-20 19:57:52 -0600

    Cambios CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7(8g859yyg4(f!$_st4u@vre*bvc&ovr@9=m)4rtku$q+h=cq-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 

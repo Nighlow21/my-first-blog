b3b6c3ab91546fe28ede4bea0c4747cc60a06929
Ordo <jaordonezdeleon2007@elvallecolegio.edu.gt>
2024-07-22 12:22:48 -0600

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    # otras URLs
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.post_list, name='post_list'),
+]
